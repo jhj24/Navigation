c3b939a3fae700eb2ccd4af2bb79a8f0e27aedd5
jhj24 <1406929846@qq.com>
2019-03-03 21:00:02 +0800

    Initial commit

---
+++ app/src/main/java/com/jhj/navigation/fragment/FragmentTwo.kt
@@ -0,0 +1,15 @@
+package com.jhj.navigation.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jhj.navigation.R
+
+class FragmentTwo : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_three, container, false)
+    }
+}
