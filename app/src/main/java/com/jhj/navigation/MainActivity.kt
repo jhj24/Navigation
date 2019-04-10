bd0381ca13eed324d4e453e967ccd074ae35ebd0
jhj <1406929846@qq.com>
2019-04-10 10:48:33 +0800

    commit

--- app/src/main/java/com/jhj/navigation/MainActivity.kt
+++ app/src/main/java/com/jhj/navigation/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.jhj.navigation
 
-import android.support.v7.app.AppCompatActivity
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        navigation_view.itemTextColor = selected(0xffff00000.toInt(), 0xff00ffff.toInt())
 
         navigation_view.setOnNavigationItemSelectedListener {
             when (it.itemId) {
@@ -41,4 +42,12 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(navigation_view, navHostFragment.navController)
     }
 
+
+    fun selected(selectedColor: Int, normalColor: Int): ColorStateList {
+        val states = arrayOfNulls<IntArray>(2)
+        states[0] = intArrayOf(android.R.attr.state_selected, android.R.attr.state_enabled)
+        states[1] = intArrayOf()
+        val colors = intArrayOf(selectedColor, normalColor)
+        return ColorStateList(states, colors)
+    }
 }
